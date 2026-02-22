2266fc9f2926916c14eaff2b34feb8760149008a
irfan1505 <86258063+irfan1505@users.noreply.github.com>
2026-02-22 15:16:09 +0530

    Update img2pdf.py

--- img2pdf.py
+++ img2pdf.py
@@ -1,3 +1,15 @@
+import os
+import mysql.connector
+
+db = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASS"),
+    database=os.getenv("DB_NAME")
+)
+
+print("✅ DB Connected Successfully from App")
+
 from flask import Flask, request, send_file, render_template_string
 from PIL import Image
 from io import BytesIO
